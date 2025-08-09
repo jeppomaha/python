8b62fe04589d6387d4abf110d7812d4433842898
jeppomaha <jadenepp@unomaha.edu>
2025-08-08 20:30:31 -0500

    Create television.py

---
+++ television.py
@@ -0,0 +1,61 @@
+class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 3
+
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
+        self.__prev_volume = self.__volume
+
+    def power(self):
+        if self.__status == False:
+            self.__status = True
+        else:
+            self.__status = False
+
+    def mute(self):
+        if self.__status:
+            if self.__muted == False:
+                self.__muted = True
+                self.__prev_volume = self.__volume
+                self.__volume = Television.MIN_VOLUME
+            else:
+                self.__muted = False
+                self.__volume = self.__prev_volume
+
+    def channel_up(self):
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                self.__channel = Television.MIN_CHANNEL
+
+    def channel_down(self):
+        if self.__status:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__channel -= 1
+            else:
+                self.__channel = Television.MAX_CHANNEL
+
+    def volume_up(self):
+        if self.__status:
+            self.__muted = False
+            self.__volume = self.__prev_volume
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
+                self.__prev_volume = self.__volume
+
+    def volume_down(self):
+        if self.__status:
+            self.__muted = False
+            self.__volume = self.__prev_volume
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
+                self.__prev_volume = self.__volume
+
+    def __str__(self):
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
